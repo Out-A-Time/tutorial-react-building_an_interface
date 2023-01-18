6dd7e095a9e2590814717de45252ccb37bf545e1
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-01-18 20:05:39 +0000

    building interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppoinment from "./components/AddAppoinment";
+import AppoinmentInfo from "./components/AppoinmentInfo";
+import appointmentList from "./data.json";
+
 function App() {
   return (
-    <div className="App">
-      <h1>YOUR CLEAN REACT APP</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top" />
+        Your Appointments
+      </h1>
+      <AddAppoinment />
+      <Search />
+
+      <ul className="divide-y divide-gray-200">
+        {appointmentList.map((appointment) => (
+          <AppoinmentInfo key={appointment.id} appointment={appointment} />
+        ))}
+      </ul>
     </div>
   );
 }
