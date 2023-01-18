e8cfedb481b92536288579538650da1bfaa1b70a
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-01-18 20:29:04 +0000

    Ready for Chapter 4. Mutating Data

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,10 @@
 import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi";
+import { useState } from "react";
 
-const DropDown = () => {
+const DropDown = ({ toggle }) => {
+  if (!toggle) {
+    return null;
+  }
   return (
     <div
       className="origin-top-right absolute right-0 mt-2 w-56
@@ -48,6 +52,8 @@ const DropDown = () => {
 };
 
 const Search = () => {
+  let [toggleSort, setToggleSort] = useState(false);
+
   return (
     <div className="py-5">
       <div className="mt-1 relative rounded-md shadow-sm">
@@ -67,6 +73,9 @@ const Search = () => {
           <div>
             <button
               type="button"
+              onClick={() => {
+                setToggleSort(!toggleSort);
+              }}
               className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
               id="options-menu"
               aria-haspopup="true"
@@ -74,7 +83,7 @@ const Search = () => {
             >
               Sort By <BiCaretDown className="ml-2" />
             </button>
-            <DropDown />
+            <DropDown toggle={toggleSort} />
           </div>
         </div>
       </div>
